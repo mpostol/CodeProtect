24f5e19c4de305f19a6092709ed4ae93f03b413f
mpostol <mpostol@cas.eu>
2020-02-02 20:31:16 +0100

    Remove dependency on external module CommonResources #7
    - updated solution metadata
    - added SolutionMetadataTemplate.t4 template
    - removed definition of the external modules

--- CAS.CodeProtect/Properties/AssemblyInfo.cs
+++ CAS.CodeProtect/Properties/AssemblyInfo.cs
@@ -1,48 +1,31 @@
-﻿//<summary>
-//  Title   : Assembly info for: CAS.CodeProtect
-//  System  : Microsoft Visual C# .NET
-//  $LastChangedDate: 2014-10-17 23:06:15 +0200 (Pt, 17 paź 2014) $
-//  $Rev:  $
-//  $LastChangedBy:  $
-//  $URL:  $
-//  $Id:  $
+﻿//___________________________________________________________________________________
 //
-//  Copyright (c) 2017 CAS LODZ POLAND
-//  +48 (42) 686 25 47
-//  techsupp@cas.eu
-//  www.cas.eu
-//</summary>
+//  Copyright (C) 2020, Mariusz Postol LODZ POLAND.
+//
+//  To be in touch join the community at GITTER: https://gitter.im/mpostol/OPC-UA-OOI
+//___________________________________________________________________________________
 
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("CAS.CodeProtect")]
-[assembly: AssemblyDescription("CAS Framework Core: CAS.CodeProtect")]
+[assembly: AssemblyTitle("Licenses creation and validation library")]
+[assembly: AssemblyDescription("Helper library supporting licenses creation and validation.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("CAS")]
-[assembly: AssemblyProduct("CAS Framework Core")]
-[assembly: AssemblyCopyright("Copyright (c) 2017 CAS LODZ POLAND")]
-[assembly: AssemblyTrademark("CAS Framework")]
+[assembly: AssemblyCompany("Mariusz Postol")]
+[assembly: AssemblyProduct("Process Observer")]
+[assembly: AssemblyCopyright("Copyright (C) 2020, Mariusz Postol LODZ POLAND.")]
+[assembly: AssemblyTrademark("Object-Oriented Internet")]
 [assembly: AssemblyCulture("")]
-
 [assembly: ComVisible(false)]
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5160a380-4fa0-4dd7-9242-1a55c295f3ed")]
-[assembly: AssemblyVersion("6.1.3.*")]
-[assembly: AssemblyFileVersion("6.1.3")]
-
+[assembly: AssemblyVersion("7.0.0.*")]
+[assembly: AssemblyFileVersion("7.0.0")]
 [assembly: System.CLSCompliantAttribute(true)]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo( "CAS.CodeProtect.UnitTests, " +  
-  "PublicKey=0024000004800000940000000602000000240000525341310004000001000100b3cff1bec656a6"+
-            "e3784c75cacce8ee3bc695d0ce135d820c86465eb49b2353f0438d28ad9a458e5350c0c141393d"+
+  "PublicKey= 0024000004800000940000000602000000240000525341310004000001000100b3cff1bec656a6"+
+             "e3784c75cacce8ee3bc695d0ce135d820c86465eb49b2353f0438d28ad9a458e5350c0c141393d"+
              "d9ad606bd9f4e0a8e1b0c49fa37835630b16798ece56df81c756f4c6bd37f07d68cc91ebfa7be7"+
              "8149d016bbc6a364beda14fc1119698d8c8d9923805b40122bf594f08cc29f41cb0a31e5e141e3"+
              "f2421b9d" )]
-[assembly: InternalsVisibleTo( "LicenseContainer.UT, " +
-  "PublicKey=0024000004800000940000000602000000240000525341310004000001000100b3cff1bec656a6"+
-            "e3784c75cacce8ee3bc695d0ce135d820c86465eb49b2353f0438d28ad9a458e5350c0c141393d"+
-            "d9ad606bd9f4e0a8e1b0c49fa37835630b16798ece56df81c756f4c6bd37f07d68cc91ebfa7be7"+
-            "8149d016bbc6a364beda14fc1119698d8c8d9923805b40122bf594f08cc29f41cb0a31e5e141e3"+
-            "f2421b9d")]
 
